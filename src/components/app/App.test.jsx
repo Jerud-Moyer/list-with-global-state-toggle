5c9dfb1fbf7890d1cefe2c33d6192d9b1f8d4709
Jerud-Moyer <jerudmoyer@gmail.com>
2020-10-29 14:20:05 -0700

    tests working

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import App from './App';
+import { ThemeProvider } from '../../context/ThemeContext';
 
 describe('App component', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
+    const { asFragment } = render(
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- src/components/header/Header.test.jsx
+++ src/components/header/Header.test.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import Header from './Header';
+import { ThemeProvider } from '../../context/ThemeContext';
 
 describe('CharacterList component', () => {
   afterEach(() => cleanup());
 
   it('renders Character', () => {
-    const { asFragment } = render(
+    const { asFragment } = render(<ThemeProvider>
       <Header/>
+    </ThemeProvider>
     );
     expect(asFragment()).toMatchSnapshot();
   });
